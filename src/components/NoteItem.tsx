4ee03c26604dfbdfefe2e06963b1739108cd7609
tobybase <74760266+tobybase@users.noreply.github.com>
2022-05-22 08:18:08 +0800

    add edit items function

--- src/components/NoteItem.tsx
+++ src/components/NoteItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AddProperty } from '../models/models';
 import { AiOutlineEdit, AiFillDelete } from 'react-icons/ai';
 import { MdDone } from 'react-icons/md';
@@ -11,6 +11,9 @@ type Props = {
 };
 
 const NoteItem = ({ item, toAdds, setToAdds }: Props) => {
+  const [edit, setEdit] = useState<Boolean>(false);
+  const [editToAdd, setEditToAdd] = useState<string>(item.toAdd);
+
   const handleDone = (id: number) => {
     setToAdds(
       toAdds.map((item) =>
@@ -19,18 +22,48 @@ const NoteItem = ({ item, toAdds, setToAdds }: Props) => {
     );
   };
 
+  const handleDelete = (id: number) => {
+    setToAdds(toAdds.filter((item) => item.id !== id));
+  };
+
+  const handleEdit = (event: React.FormEvent, id: number) => {
+    event.preventDefault();
+
+    setToAdds(
+      toAdds.map((item) =>
+        item.id === id ? { ...item, item: editToAdd } : item
+      )
+    );
+    setEdit(false);
+  };
+
   return (
-    <form className='items__single'>
-      {item.isDone ? (
+    <form
+      className='items__single'
+      onSubmit={(event) => handleEdit(event, item.id)}>
+      {edit ? (
+        <input
+          value={editToAdd}
+          onChange={(event) => setEditToAdd(event.target.value)}
+          className='items__single--test'
+        />
+      ) : item.isDone ? (
         <s className='items__single--text'>{item.toAdd}</s>
       ) : (
         <span className='items__single--text'>{item.toAdd}</span>
       )}
+
       <div>
-        <span className='icon'>
+        <span
+          className='icon'
+          onClick={() => {
+            if (!edit && !item.isDone) {
+              setEdit(!edit);
+            }
+          }}>
           <AiOutlineEdit />
         </span>
-        <span className='icon'>
+        <span className='icon' onClick={() => handleDelete(item.id)}>
           <AiFillDelete />
         </span>
         <span className='icon' onClick={() => handleDone(item.id)}>
